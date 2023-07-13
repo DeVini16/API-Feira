b198e16ed51b23e63e618a0c5cf9371a7dcb3172
Vinicius Alves <vinalves2003@gmail.com>
2023-07-12 22:04:11 -0300

    implementação de bd

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const express = require('express');
+import {openDb} from './configBD.js';
+import express from 'express';
+
 const app = express()
 app.use(express.json());
+
+openDb();
+
+const apt = "helloooo" 
 
 app.get('/', (req, res) => {
     res.send("buscando feiras")
@@ -8,9 +14,17 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-    res.send("Inserindo feiras")
+    res.json({
+        "palavraCampos": apt
+    })
 
 })
+
+
+
+
+
+
 app.listen(3000, () => {
     console.log("Rodando!")
 })
